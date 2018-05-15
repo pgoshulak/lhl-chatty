3e9e18b7c0c8f0330783d1d8ed46a7a962e82a62
Peter Goshulak <pgoshulak@gmail.com>
2018-05-15 12:37:15 -0400

    Change Message, MessageList into functional components

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Message extends Component {
-  render() {
-    const { type, content, username } = this.props.messageData
-    return (
-      type === 'incomingNotification'
-        ? <div className="message system">{content}</div>
-        : (
-          <div className='message'>
-            <span className="message-username">{username}</span>
-            <span className="message-content">{content}</span>
-          </div>
-        )
-    )
-  }
+const Message = ({ message }) => {
+  const { type, content, username } = message
+  return (
+    type === 'incomingNotification'
+      ? <div className="message system">{content}</div>
+      : (
+        <div className='message'>
+          <span className="message-username">{username}</span>
+          <span className="message-content">{content}</span>
+        </div>
+      )
+  )
 }
 
+
 Message.propTypes = {
-  messageData: PropTypes.shape({
+  message: PropTypes.shape({
     id: PropTypes.any,
     type: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Message from './Message.jsx';
 import PropTypes from 'prop-types';
 
-class MessageList extends Component {
-  render() { 
-    let messages = this.props.messages.map(message => (<Message key={message.id} messageData={message} />));
-    return (
-      <main className='messages'>
-        { messages }
-      </main> 
-    )
-  }
+const MessageList = ({ messages }) => {
+  return (
+    <main className='messages'>
+      { messages.map(message => 
+        (<Message key={message.id} message={message} />)
+      ) }
+    </main> 
+  )
 }
+
  
 MessageList.propTypes = {
   messages: PropTypes.array
