19b922174a0cf049b60b70ea14a7e46fb190b8aa
Peter Goshulak <pgoshulak@gmail.com>
2018-05-17 13:06:11 -0400

    Add parser to display message content img URLs as images

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const ImageContent = ({imgUrl}) => {
+    // Small image should not resize:
+  // https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Grumpy_Cat_by_Gage_Skidmore.jpg/220px-Grumpy_Cat_by_Gage_Skidmore.jpg
+  // Large image should resize down to max 60vw: 
+  // http://getwallpapers.com/wallpaper/full/4/3/5/739783-download-cute-cat-background-1920x1080-for-hd.jpg
+  if (!imgUrl) {
+    return null
+  } else {
+    const containerStyle = {
+      maxWidth: '60vw'
+    };
+    const imgStyle = {
+      maxWidth: '100%',
+      display: 'block'
+    }
+    return (
+      <div style={containerStyle}>
+        <img src={imgUrl} style={imgStyle} alt="Image"/>
+      </div>
+    )
+  }
+}
+
 const Message = ({ message }) => {
   const { type, content, username, color } = message
   const style = {
     color: color
   }
+  // Extract image URL and convert to <ImageContent/>
+  let imgUrl = null;
+  let formattedContent = content;
+  const imageUrlRegex = RegExp(/.*(https?\:\/\/\S+\.(jpg|jpeg|png|gif)).*/, 'g');
+  const match = imageUrlRegex.exec(content)
+  if (match) {
+    // Save the url itself
+    imgUrl = match[1]
+    // Remove the image url from the message itself
+    formattedContent = formattedContent.replace(imgUrl, '')
+  }
+
   return (
     type === 'incomingNotification'
       /* Notification, eg. username changes */
@@ -14,7 +49,10 @@ const Message = ({ message }) => {
       : (
         <div className='message'>
           <span className="message-username" style={style}>{username}</span>
-          <span className="message-content">{content}</span>
+          <span className="message-content">
+            {formattedContent}
+            <ImageContent imgUrl={imgUrl}/>
+          </span>
         </div>
       )
   )
@@ -28,6 +66,10 @@ Message.propTypes = {
     username: PropTypes.string,
     color: PropTypes.string
   })
+}
+
+ImageContent.propTypes = {
+  imgUrl: PropTypes.string
 }
 
 export default Message;
